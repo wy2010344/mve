43c077686044c27508d8454c005a6c59424a216e
wangy <1179221923@qq.com>
2018-08-14 18:12:41 +0800

    为S-Lisp增加一些库函数，主要是列表操作的，调整mve，包括S-Lisp中的mve，支持children属性；js中的mve，与S-Lisp尽量保持一致（但感觉还是js简单得多。。。。）

--- public/index/S-Lisp/index/index.js
+++ public/index/S-Lisp/index/index.js
@@ -5,52 +5,15 @@
 		util:"../util.js"
 	},
 	success:function(){
-		return mve(function(me){
-			var x=lib.load(
-				lib.library(
-					function(cs) {
-						mb.log.apply(null,cs);
-					}
-				)
-			);
-			/*
-			lib.load(lib.library(),function(load,scope){
-				load(pkg.required_url,"./index.lisp",function(r,err){
-					try{
-						mb.log(r,err);
-						var m=lib.util.map_from_kvs(r);
-						m.getElement.exec(null,function(el){
-							//mb.log(el);
-							me.k.root.appendChild(el);
-						});
-					}catch(e){
-						mb.log(e);
-					}
-				});
-			});
-			*/
-			return {
-				init:function() {
-					mb.log("init")
-				},
-				element:{
-					type:"div",
-					id:"root",
-					children:[
-						{
-							type:function() {
-								var r=x.load(pkg.required_url,"./index.lisp");
-								var m=lib.util.map_from_kvs(r);
-								/*
-								m.init=function() {
-									mb.log("自定义init")
-								};*/
-								return m;
-							}
-						}
-					]
+		var x=lib.load(
+			lib.library(
+				function(cs) {
+					mb.log.apply(null,cs);
 				}
-			};
-		});
+			)
+		);
+		var r=x.load(pkg.required_url,"./index.lisp");
+		var m=lib.util.map_from_kvs(r);
+		return m;
 	}
 });
