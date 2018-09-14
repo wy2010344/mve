01dd5f4dd340afc39e8ebbd4bf219a4bfae1d334
wy2010344 <wangyang2010344@foxmail.com>
2018-09-14 21:45:47 +0800

    S-Reader，代码小调整

---
+++ S-Reader/S-Reader/FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace S_Reader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.browserMain = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // browserMain
+            // 
+            this.browserMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserMain.Location = new System.Drawing.Point(0, 0);
+            this.browserMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browserMain.Name = "browserMain";
+            this.browserMain.Size = new System.Drawing.Size(717, 533);
+            this.browserMain.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 533);
+            this.Controls.Add(this.browserMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browserMain;
+    }
+}
+
---
+++ public/index/s-view.js
@@ -0,0 +1,54 @@
+({
+	data:{
+		SLisp:"./S-Lisp/index.js",
+		interpret:"./S-Lisp/interpret.js",
+		s:"./S-Lisp/s.js",
+		mve:{
+			url:"./S-Lisp/util/mve/index.lisp",
+			type:"path"
+		}
+	},
+	success:function() {
+		if(cp.query.path){
+			var r=lib.SLisp.run(
+				cp.query.path,
+				[
+					{
+						url:lib.mve,
+						delay:true,
+						key:"mve"
+					}
+				],
+				function(path,txt) {
+					if(path.endsWith("s-view")){
+						txt="{(mve {"+txt+"})}";
+					}
+					return {
+						txt:txt
+					};
+				}
+			);
+			return mve(function(me) {
+				return {
+					element:{
+						type:function() {
+							var o=r.exec(null);
+							var x=lib.s.map_from_kvs(o);
+							document.title=x.title||"";
+							return x;
+						}
+					}
+				};
+			});
+		}else{
+			return mve(function(me) {
+				return {
+					element:{	
+						type:"div",
+						text:"期待一个文件"
+					}
+				};
+			});
+		}
+	}
+});
