9cd988b8f643048d1236979b89e157132ba21816
wangy <1179221923@qq.com>
2019-01-26 20:19:30 +0800

    调整mve，为了兼容别的平台

--- public/lib/mve/parse/buildChildren.js
+++ public/lib/mve/parse/buildChildren.js
@@ -1,17 +1,28 @@
 ({
+    data:{
+        nokey:"./nokey.js"
+    },
     /**
      * 改变行的data()，需要逆向改变数组
+
+     Value
+     Watcher
+     key
+     appendChild
+     removeChild
+     before 可选
+     after 可选
      */
-    success:function(Value,Watcher,DOM,nokey){
-        var build=function(repeat,mvvm){
+    success:function(p){
+        var build=function(repeat,mve){
             return function(row,i){
                 var o={
-                    data:Value(row),
-                    index:Value(i)
+                    data:p.Value(row),
+                    index:p.Value(i)
                 };
                 var value={
                     row:o,
-                    obj:mvvm(function(me){
+                    obj:mve(function(me){
                         return {
                             element:function(){
                                 return repeat(o);
@@ -22,64 +33,99 @@
                 return value;
             };
         };
-        var buildChildren=function(pel,children,inits,destroys,mvvm){
-            var c_inits=[];
-            var isInit=false;
-            var bc=nokey({
-                build:build(children.repeat,mvvm),
-                after:function(value){
-                    if(isInit){
-                        //直接初始化
-                        if(value.obj.init){
-                            value.obj.init();
-                        }
+        var p_before=p.before||mb.emptyFunc;
+        var p_after=p.after||mb.emptyFunc;
+        var buildChildren=function(pel,x,o){
+            var children=o.json[p.key];
+            if(children){
+                if(typeof(children)=='object') {
+                    var inits=o.inits;
+                    var destroys=o.destroys;
+                    if (mb.Array.isArray(children)) {
+                        //普通的
+                        return mb.Array.reduce(
+                            children,
+                            function(init,child){
+                                init.json=child;
+                                var obj=x.Parse(x,init);
+                                p.appendChild(pel,obj.element);
+                                return obj;
+                            },
+                            {
+                                k:o.k,
+                                inits:inits,
+                                destroys:destroys
+                            }
+                        );
                     }else{
-                        //附加于父层去初始化
-                        if(value.obj.init){
-                            c_inits.push(value.obj.init);
+                        //非普通的
+                        var c_inits=[];
+                        var isInit=false; 
+                        var bc=lib.nokey({
+                            build:build(children.repeat,x.mve),
+                            after:function(value){
+                                var init=value.obj.init;
+                                if(isInit){
+                                    //直接初始化
+                                    init();
+                                }else{
+                                    //附加于父层去初始化
+                                    c_inits.push(init);
+                                }
+                            },
+                            update_data:function(value,v){
+                                value.row.data(v);
+                            },
+                            destroy:function(value){
+                                value.obj.destroy();
+                            },
+                            appendChild:function(value){
+                                p.appendChild(pel,value.obj.getElement());
+                            },
+                            removeChild:function(value){
+                                p.removeChild(pel,value.obj.getElement());
+                            }
+                        });
+                        /*
+                        暂时不考虑区分 
+                        if (children.key) {
+                            //bc=lib.key(p,parseRow,buildOne,pel,children);
+                        }else{
                         }
+                        */
+                        var watch=p.Watcher({
+                            before:function(){
+                                p_before(pel);
+                            },
+                            exp:function(){
+                                return children.array();
+                            },
+                            after:function(array){
+                                bc.after(array);
+                                p_after(pel);
+                            }
+                        });
+                        //初始化、销毁附加到全局
+                        inits.push(function() {
+                            mb.Array.forEach(c_inits,function(c_i) {
+                                c_i();
+                            });
+                            isInit=true;
+                        });
+                        destroys.push(function() {
+                            watch.disable();
+                            bc.destroy();
+                        });
+                        return {
+                            k:o.k,
+                            inits:inits,
+                            destroys:destroys
+                        };
                     }
-                },
-                update_data:function(value,v){
-                    value.row.data(v);
-                },
-                destroy:function(value){
-                    value.obj.destroy();
-                },
-                appendChild:function(value){
-                    DOM.appendChild(pel,value.obj.getElement());
-                },
-                removeChild:function(value){
-                    DOM.removeChild(pel,value.obj.getElement());
                 }
-            });
-            /*
-            暂时不考虑区分 
-            if (children.key) {
-                //bc=lib.key(p,parseRow,buildOne,pel,children);
             }else{
+                return o;
             }
-            */
-
-            var watch=Watcher({
-                exp:function(){
-                    return children.array();
-                },
-                after:function(array){
-                    bc.after(array);
-                }
-            });
-            //初始化、销毁附加到全局
-            inits.push(function() {
-                mb.Array.forEach(c_inits,function(c_i) {
-                    c_i();
-                });
-                isInit=true;
-            });
-            destroys.push(function() {
-                watch.disable();
-                bc.destroy();
-            });
         };
         return buildChildren;
     }
