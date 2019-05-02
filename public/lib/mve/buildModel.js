dc548936e878aa26bf9a127d0701991d647b2cc2
wangy <1179221923@qq.com>
2019-05-02 20:35:59 +0800

    mve-repeat更科学的使用方式

--- public/lib/mve/buildModel.js
+++ public/lib/mve/buildModel.js
@@ -1,9 +1,9 @@
 ({
 	/**
 	*
-    build
-    model
-    update_index(view,index);
+	build
+	model
+	update_index(view,index);
 	insertChildBefore(new_view,old_view)
 	appendChild(view)
 	removeChild(view)
@@ -11,68 +11,68 @@
 	destroy(view)
 	*/
 	success:function(p){
-        var views=[];
-        var view={
-            insert:function(index,row){
-                //动态加载
-                var view=p.build(row,index);
-                views.splice(index,0,view);
-                for(var i=index+1;i<views.length;i++){
-                    p.update_index(views[i],i);
-                }
-                if(index<views.length-1){
-                    /*移动到前面*/
-                    p.insertChildBefore(view,views[index+1]);
-                }else{
-                    p.appendChild(view)
-                }
-                p.init(view);
-            },
-            remove:function(index){
-                //动态删除
-                var view=views[index];
-                views.splice(index,1);
-                for(var i=index;i<views.length;i++){
-                    p.update_index(views[i],i);
-                }
-                p.destroy(view);
-                p.removeChild(view);
-            },
-            move:function(old_index,new_index){
-                var view=views[old_index];
-                views.splice(old_index,1);
-                views.splice(new_index,0,view);
+		var views=[];
+		var view={
+			insert:function(index,row){
+				//动态加载
+				var view=p.build(row,index);
+				views.splice(index,0,view);
+				for(var i=index+1;i<views.length;i++){
+					p.update_index(views[i],i);
+				}
+				if(index<views.length-1){
+					/*移动到前面*/
+					p.insertChildBefore(view,views[index+1]);
+				}else{
+					p.appendChild(view)
+				}
+				p.init(view);
+			},
+			remove:function(index){
+				//动态删除
+				var view=views[index];
+				views.splice(index,1);
+				for(var i=index;i<views.length;i++){
+					p.update_index(views[i],i);
+				}
+				p.destroy(view);
+				p.removeChild(view);
+			},
+			move:function(old_index,new_index){
+				var view=views[old_index];
+				views.splice(old_index,1);
+				views.splice(new_index,0,view);
 
-                var sort=old_index<new_index?[old_index,new_index]:[new_index,old_index];
-                for(var i=sort[0];i<sort[1];i++){
-                    p.update_index(views[i],i);
-                }
-                
-                if (new_index<views.length-1) {
-                    p.insertChildBefore(view,views[new_index+1]);
-                }else{
-                    p.appendChild(view);
-                }
-            }
-        };
-        p.model.addView(view);
-        var initViews=[];
-        for(var i=0;i<p.model.size();i++){
-            var row=p.model.get(i);
-            var view=p.build(row,i);
-            views.push(view);
-            p.appendChild(view);
-            initViews.push(view);
-        }
+				var sort=old_index<new_index?[old_index,new_index]:[new_index,old_index];
+				for(var i=sort[0];i<=sort[1];i++){
+					p.update_index(views[i],i);
+				}
+				
+				if (new_index<views.length-1) {
+					p.insertChildBefore(view,views[new_index+1]);
+				}else{
+					p.appendChild(view);
+				}
+			}
+		};
+		p.model.addView(view);
+		var initViews=[];
+		for(var i=0;i<p.model.size();i++){
+			var row=p.model.get(i);
+			var view=p.build(row,i);
+			views.push(view);
+			p.appendChild(view);
+			initViews.push(view);
+		}
 
-        return {
-        	init:function(){
-        		mb.Array.forEach(initViews,p.init);
-        	},
-        	destroy:function(){
-	            mb.Array.forEach(views,p.destroy);
-	            p.model.removeView(view);
-        	}
-        };
-    }
+		return {
+			init:function(){
+				mb.Array.forEach(initViews,p.init);
+			},
+			destroy:function(){
+				mb.Array.forEach(views,p.destroy);
+				p.model.removeView(view);
+			}
+		};
+	}
 })
--- public/lib/mve/parse.js
+++ public/lib/mve/parse.js
@@ -1,155 +1,155 @@
 ({
-    delay:true,
-    success:function(){
-        var bindFactory=function(watch){
-            return function(value,f){
-                if(typeof(value)=="function"){
-                    //observe
-                    watch({
-                        exp:function(){
-                            return value();
-                        },
-                        after:function(v){
-                            f(v);
-                        }
-                    });
-                }else{
-                    f(value);
-                }
-            };
-        };
-        var if_bind=function(bind) {
-            return function(value,f){
-                if(value!=undefined){
-                    bind(value,f);
-                } 
-            };
-        };
-        var add_k=function(k,id,obj){
-            if (id) {
-                var old=k[id];
-                if(old!=null){
-                    mb.log("已经存在重复的定义"+id,old,"不能添加",obj);
-                }else{
-                    k[id]=obj;
-                }
-            }
-            return k;
-        };
-        return function(p){
-            var ParseFun=function(x,o){
-                var change=mb.cache();
-                x.watch({
-                    exp:function(){
-                       return o.json(); 
-                    },
-                    after:function(jo){
-                        var newObj=x.mve(function(){
-                            return {
-                                element:jo
-                            };
-                        })(o.e);
-                        var obj=change();
-                        change(newObj);
-                        if(obj){
-                            //非第一次生成
-                            o.e.replaceChild(o.e,obj.element,newObj.element);
-                            obj.destroy();
-                            newObj.init();
-                        }
-                    }
-                });
-                //最后一个销毁
-                o.destroys.push(function() {
-                    var destroy=change().destroy||mb.Function.quote.one;
-                    destroy();
-                });
-                o.inits.push(change().init);
-                return {
-                    change:change,
-                    inits:o.inits,
-                    destroys:o.destroys
-                };
-            };
-            var ParseObject=function(x,o){
-                var json=o.json||"";
-                if(typeof(json)!="object"){
-                    return p.createTextNode(x,o);
-                }else{
-                    var type=o.json.type;
-                    var id=o.json.id;
-                    if(typeof(type)=="string"){
-                        var obj=p.buildElement(x,o);
-                        var el=obj.element;
-                        return {
-                            element:el,
-                            k:add_k(obj.k,id,el),
-                            inits:obj.inits,
-                            destroys:obj.destroys
-                        };
-                    }else{
-                        var obj=type(json)(o.e);
-                        var el=obj.element;
-                        /*虽然mve模板是有init与destroy，但原生模块并没有*/
-                        if(obj.init){
-                            o.inits.push(obj.init);
-                        }
-                        if(obj.destroy){
-                            o.destroys.push(obj.destroy);
-                        }
-                        return {
-                            element:el,
-                            k:add_k(o.k,id,obj),
-                            inits:o.inits,
-                            destroys:o.destroys
-                        };
-                    }
-                };
-            };
-            var Parse=function(x,o){
-                if(typeof(o.json)=="function"){
-                    var vm=ParseFun(x,o);
-                    return {
-                        element:vm.change().element,
-                        k:o.k,
-                        inits:vm.inits,
-                        destroys:vm.destroys
-                    };
-                }else{
-                    return ParseObject(x,o);
-                }
-            };
-            return function(e,json,watch,mve,k){
-                /*不变的*/
-                var bind=bindFactory(watch);
-                var x={
-                    Parse:Parse,
-                    watch:watch,
-                    mve:mve,
-                    bind:bind,
-                    if_bind:if_bind(bind)
-                };
-                /*变化的*/
-                var o={
-                    json:json,
-                    e:e,
-                    k:{},
-                    inits:[],
-                    destroys:[]
-                };
-                if(typeof(o.json)=='function'){
-                    //根是function，要更新最新的el。
-                    var vm=ParseFun(x,o);
-                    return {
-                        element:vm.change().element,
-                        k:{},
-                        inits:vm.inits,
-                        destroys:vm.destroys
-                    };
-                }else{
-                    return ParseObject(x,o);
-                }
-            };
-        };
-    }
+	delay:true,
+	success:function(){
+		var bindFactory=function(watch){
+			return function(value,f){
+				if(typeof(value)=="function"){
+					//observe
+					watch({
+						exp:function(){
+							return value();
+						},
+						after:function(v){
+							f(v);
+						}
+					});
+				}else{
+					f(value);
+				}
+			};
+		};
+		var if_bind=function(bind) {
+			return function(value,f){
+				if(value!=undefined){
+					bind(value,f);
+				} 
+			};
+		};
+		var add_k=function(k,id,obj){
+			if (id) {
+				var old=k[id];
+				if(old!=null){
+					mb.log("已经存在重复的定义"+id,old,"不能添加",obj);
+				}else{
+					k[id]=obj;
+				}
+			}
+			return k;
+		};
+		return function(p){
+			var ParseFun=function(x,o){
+				var change=mb.cache();
+				x.watch({
+					exp:function(){
+					   return o.json(); 
+					},
+					after:function(jo){
+						var newObj=x.mve(function(){
+							return {
+								element:jo
+							};
+						})(o.e);
+						var obj=change();
+						change(newObj);
+						if(obj){
+							//非第一次生成
+							o.e.replaceChild(o.e,obj.element,newObj.element);
+							obj.destroy();
+							newObj.init();
+						}
+					}
+				});
+				//最后一个销毁
+				o.destroys.push(function() {
+					var destroy=change().destroy||mb.Function.quote.one;
+					destroy();
+				});
+				o.inits.push(change().init);
+				return {
+					change:change,
+					inits:o.inits,
+					destroys:o.destroys
+				};
+			};
+			var ParseObject=function(x,o){
+				var json=o.json||"";
+				if(typeof(json)!="object"){
+					return p.createTextNode(x,o);
+				}else{
+					var type=o.json.type;
+					var id=o.json.id;
+					if(typeof(type)=="string"){
+						var obj=p.buildElement(x,o);
+						var el=obj.element;
+						return {
+							element:el,
+							k:add_k(obj.k,id,el),
+							inits:obj.inits,
+							destroys:obj.destroys
+						};
+					}else{
+						var obj=type(json)(o.e);
+						var el=obj.element;
+						/*虽然mve模板是有init与destroy，但原生模块并没有*/
+						if(obj.init){
+							o.inits.push(obj.init);
+						}
+						if(obj.destroy){
+							o.destroys.push(obj.destroy);
+						}
+						return {
+							element:el,
+							k:add_k(o.k,id,obj.out),//只将out部分暴露出去
+							inits:o.inits,
+							destroys:o.destroys
+						};
+					}
+				};
+			};
+			var Parse=function(x,o){
+				if(typeof(o.json)=="function"){
+					var vm=ParseFun(x,o);
+					return {
+						element:vm.change().element,
+						k:o.k,
+						inits:vm.inits,
+						destroys:vm.destroys
+					};
+				}else{
+					return ParseObject(x,o);
+				}
+			};
+			return function(e,json,watch,mve,k){
+				/*不变的*/
+				var bind=bindFactory(watch);
+				var x={
+					Parse:Parse,
+					watch:watch,
+					mve:mve,
+					bind:bind,
+					if_bind:if_bind(bind)
+				};
+				/*变化的*/
+				var o={
+					json:json,
+					e:e,
+					k:{},
+					inits:[],
+					destroys:[]
+				};
+				if(typeof(o.json)=='function'){
+					//根是function，要更新最新的el。
+					var vm=ParseFun(x,o);
+					return {
+						element:vm.change().element,
+						k:{},
+						inits:vm.inits,
+						destroys:vm.destroys
+					};
+				}else{
+					return ParseObject(x,o);
+				}
+			};
+		};
+	}
 })
