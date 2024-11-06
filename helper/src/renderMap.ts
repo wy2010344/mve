5db34d15f8fc672805616fa20d21d33f6ba2a8e8
wyknmjj <wyknmjj@protonmail.com>
2024-11-06 18:36:01 +0800

    save change

--- helper/src/renderMap.ts
+++ helper/src/renderMap.ts
@@ -1,5 +1,5 @@
 import { renderForEach } from "mve-core"
-import { GetValue, ReadArray, RMap } from "wy-helper"
+import { GetValue, memo, normalMapCreater, ReadArray, RMap, run } from "wy-helper"
 
 
 export type ItemWithIndex<T> = {
@@ -24,4 +24,58 @@ export function renderArray<T, K>(
         })
       }
     }, createMap)
+}
+export function renderArrayToMap<T, K, O>(
+  getVs: GetValue<ReadArray<T>>,
+  getKey: (v: T, i: number) => K,
+  render: (get: GetValue<Readonly<ItemWithIndex<T>>>) => O,
+  createMap: <V>() => RMap<K, V> = normalMapCreater
+): GetValue<RMap<K, GetValue<O>>> {
+  let gets: RMap<K, GetValue<O>>
+  const getVS1 = memo(getVs)
+  renderForEach<Readonly<ItemWithIndex<T>>, K, O>(
+    function (callback) {
+      const vs = getVS1()
+      gets = createMap<GetValue<O>>()
+      for (let i = 0; i < vs.length; i++) {
+        const v = vs[i]
+        const key = getKey(v, i)
+        const get = callback(key, render, {
+          index: i,
+          item: v
+        })
+        gets.set(key, get)
+      }
+    }, createMap)
+  return function () {
+    getVS1()
+    return gets
+  }
+}
+export function renderArrayToArray<T, K, O>(
+  getVs: GetValue<ReadArray<T>>,
+  getKey: (v: T, i: number) => K,
+  render: (get: GetValue<Readonly<ItemWithIndex<T>>>) => O,
+  createMap?: <V>() => RMap<K, V>
+): GetValue<O[]> {
+  let gets: GetValue<O>[]
+  const getVS1 = memo(getVs)
+  renderForEach<Readonly<ItemWithIndex<T>>, K, O>(
+    function (callback) {
+      const vs = getVS1()
+      gets = []
+      for (let i = 0; i < vs.length; i++) {
+        const v = vs[i]
+        const key = getKey(v, i)
+        const get = callback(key, render, {
+          index: i,
+          item: v
+        })
+        gets.push(get)
+      }
+    }, createMap)
+  return function () {
+    getVS1()
+    return gets.flatMap(run)
+  }
 }
