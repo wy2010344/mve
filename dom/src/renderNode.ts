8de508f68fbb49658a8a0b5c8909e0033b48ff2f
wyknmjj <wyknmjj@protonmail.com>
2024-12-06 18:42:23 +0800

    add more layout support

--- dom/src/renderNode.ts
+++ dom/src/renderNode.ts
@@ -1,6 +1,6 @@
-import { BDomEvent, BSvgEvent, DomElement, DomElementType, FChildAttr, FDomAttribute, FSvgAttribute, getAttributeAlias, renderFNodeAttr, SvgElement, SvgElementType } from "wy-dom-helper"
+import { BDomEvent, BSvgEvent, DomElement, DomElementType, FDomAttribute, FGetChildAttr, FSvgAttribute, renderFNodeAttr, SvgElement, SvgElementType } from "wy-dom-helper"
 import { OrFun, renderPortal } from "./hookChildren"
-import { emptyObject, GetValue, SetValue } from "wy-helper"
+import { emptyObject } from "wy-helper"
 import { hookTrackSignalMemo } from "mve-helper"
 import { hookAddResult } from "mve-core"
 
@@ -20,7 +20,7 @@ function mergeValue(
 
 export function renderDom<T extends DomElementType>(type: T, arg: OrFun<FDomAttribute<T>>
   & BDomEvent<T>
-  & FChildAttr<DomElement<T>> = emptyObject as any
+  & FGetChildAttr<DomElement<T>> = emptyObject as any
 ): DomElement<T> {
   const node = document.createElement(type)
   renderFNodeAttr(node, arg, "dom", mergeValue, renderPortal)
@@ -29,7 +29,7 @@ export function renderDom<T extends DomElementType>(type: T, arg: OrFun<FDomAttr
 }
 export function renderSvg<T extends SvgElementType>(type: T, arg: OrFun<FSvgAttribute<T>>
   & BSvgEvent<T>
-  & FChildAttr<SvgElement<T>> = emptyObject as any
+  & FGetChildAttr<SvgElement<T>> = emptyObject as any
 ): SvgElement<T> {
   const node = document.createElementNS("http://www.w3.org/2000/svg", type)
   renderFNodeAttr(node, arg, "svg", mergeValue, renderPortal)
