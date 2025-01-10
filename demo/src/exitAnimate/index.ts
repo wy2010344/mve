734fbf5c89a056edcd5ade55060e3f1a4850ac5e
wy2010344 <wangyang2010344@foxmail.com>
2025-01-10 18:46:39 +0800

    save change

--- demo/src/exitAnimate/index.ts
+++ demo/src/exitAnimate/index.ts
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
+import { animate } from "motion";
 import { fdom } from "mve-dom";
-import { renderArray } from "mve-helper";
-import { createSignal, emptyArray, genTemplateStringS1, GetValue } from "wy-helper";
+import { getExitAnimateArray, hookTrackSignal, renderArray } from "mve-helper";
+import { createSignal, emptyArray } from "wy-helper";
 
 export default function () {
 
@@ -18,9 +19,59 @@ export default function () {
     s_alignItems: 'center',
     children() {
 
-      renderArray(list.get, (row, getIndex) => {
-        genTemplateStringS1
-        fdom.div({
+      const modeIdx = createSignal(0)
+      const waitIdx = createSignal(0)
+      function mode() {
+        const idx = modeIdx.get() % 3
+        if (idx == 1) {
+          return 'pop'
+        } else if (idx == 2) {
+          return 'shift'
+        }
+      }
+      function wait() {
+        const idx = waitIdx.get() % 3
+        if (idx == 1) {
+          return 'in-out'
+        } else if (idx == 2) {
+          return 'out-in'
+        }
+      }
+      const getList = getExitAnimateArray(list.get, {
+        mode,
+        wait
+      })
+
+
+      fdom.div({
+        s_display: 'flex',
+        s_gap: "10px",
+        children() {
+          fdom.button({
+            childrenType: "text",
+            onClick() {
+              modeIdx.set(modeIdx.get() + 1)
+            },
+            children() {
+              return `mode: ${mode() || 'normal'}`
+            }
+          })
+          fdom.button({
+            childrenType: "text",
+            onClick() {
+              waitIdx.set(waitIdx.get() + 1)
+            },
+            children() {
+              return `wait: ${wait() || 'normal'}`
+            }
+          })
+        }
+      })
+      renderArray(getList, (row, getIndex) => {
+
+
+
+        const div = fdom.div({
           s_display: 'flex',
           s_gap: '10px',
           children() {
@@ -30,7 +81,7 @@ export default function () {
             })
             fdom.span({
               childrenType: "text",
-              children: row.time + ''
+              children: row.value.time + ''
             })
             fdom.button({
               childrenType: "text",
@@ -38,10 +89,34 @@ export default function () {
               s_paddingInline: '10px',
               s_background: 'gray',
               onClick() {
-                list.set(list.get().filter(x => x != row))
+                list.set(list.get().filter(x => x != row.value))
               }
             })
+            fdom.button({
+              childrenType: "text",
+              children: "替换",
+              s_paddingInline: '10px',
+              s_background: 'gray',
+              onClick() {
+                list.set(
+                  list.get().map(v => {
+                    if (v == row.value) {
+                      return {
+                        ...v
+                      }
+                    }
+                    return v
+                  })
+                )
+              }
+
+            })
           }
+        })
+        hookTrackSignal(row.exiting, (v) => {
+          animate(div, {
+            x: v ? [0, '100%'] : ['100%', 0]
+          }).finished.then(row.resolve)
         })
       })
 
@@ -63,12 +138,5 @@ export default function () {
       })
     }
   })
-
-}
-
-
-function renderExitAnimate<T>(
-  get: GetValue<T[]>
-) {
 
 }
