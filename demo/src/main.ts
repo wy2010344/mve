8bbf2bb8ff3edd5bd4ad3d1d9d1dd3378bf88515
wy2010344 <wangyang2010344@foxmail.com>
2025-01-16 18:50:30 +0800

    some fix

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -25,12 +25,12 @@ import exitAnimate from './exitAnimate'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 const destroy = createRoot(app, () => {
-  scrollerDemo1()
+  // scrollerDemo1()
   // reorderDemo()
   // changePortal()
   // canvas()
   // canvas2()
-  // exitAnimate()
+  exitAnimate()
   // scrollDemo1()
   // scrollDemo1p()
   // pagingx()
