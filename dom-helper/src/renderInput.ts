390659e08641a112a68a923cb8930f0b42ac99c7
wy2010344 <wangyang2010344@foxmail.com>
2025-03-15 20:21:46 +0800

    some code fix

--- dom-helper/src/renderInput.ts
+++ dom-helper/src/renderInput.ts
@@ -1,13 +1,14 @@
 import { OrFun, renderFDom } from "mve-dom";
 import { hookTrackSignal, useVersion } from "mve-helper";
 import { BDomEvent, DomElement, DomElementType, FDomAttribute } from "wy-dom-helper";
-import { batchSignalEnd, EmptyFun, emptyFun, ValueOrGet, valueOrGetToGet } from "wy-helper";
+import { HTMLContentEditableFixCache, InputEditCache, TextContentEditableFixCache } from 'wy-dom-helper/contentEditable'
+import { batchSignalEnd, EmptyFun, emptyFun, SetValue, ValueOrGet, valueOrGetToGet } from "wy-helper";
 
 
 export type TriggerTime = "onInput" | "onBlur"
 type InputTypeProps<T extends DomElementType> = OrFun<FDomAttribute<T>> & BDomEvent<T> & {
   triggerTime?: ValueOrGet<TriggerTime>,
-  value?: ValueOrGet<string>
+  value?: ValueOrGet<string | number>
   onValueChange(v: string): void
 }
 
@@ -26,37 +27,72 @@ export type InputProps = Omit<InputTypeProps<"input">, 'type'> & {
     | 'image'>>
 }
 
-function useUpdateValue<K extends string, V>(
+// //这个需要处理select-change
+// function useUpdateValue2<V>(
+//   cache: EditFixCache<any, V>,
+//   _getValue: ValueOrGet<V>,
+//   getDep: EmptyFun) {
+
+//   const getValue = valueOrGetToGet(_getValue)
+//   hookTrackSignal(() => {
+//     getDep()
+//     const value = getValue()
+//     //必须依赖旧值,更新新值,所以在0
+//     addEffect(() => {
+//       cache.change(value)
+//     })
+//   })
+// }
+
+abstract class Cache<V> {
+  abstract getValue(): V
+  abstract setValue(v: V): void
+  equals(a: V, b: V) {
+    return a === b
+  }
+}
+
+class InputCache extends Cache<string> {
+  constructor(
+    private input: HTMLInputElement
+  ) { super() }
+  getValue(): string {
+    return this.input.value
+  }
+  setValue(v: string): void {
+    this.input.value = v
+  }
+  static from(input: HTMLInputElement) {
+    return new InputCache(input)
+  }
+}
+//这个不需要处理select-change
+function useUpdateValue<V>(
   _getValue: ValueOrGet<V>,
-  input: {
-    [key in K]: V | null
-  },
-  key: K,
-  getDep: EmptyFun
+  cache: Cache<V>,
+  getDep: EmptyFun,
 ) {
   const getValue = valueOrGetToGet(_getValue)
   hookTrackSignal(() => {
     getDep()
     const value = getValue()
-    if (value != input[key]) {
+    if (value !== cache.getValue()) {
       //必须用实时值去改!!!
-      input[key] = value
+      cache.setValue(value)
     }
   })
 }
 
 function useTrigger<
-  K extends string,
-  N extends {
-    [key in K]: string | null
-  }
+  E,
+  V
 >(
   triggerTime: ValueOrGet<TriggerTime> = "onInput",
   props: Record<string, any>,
-  value: ValueOrGet<string> = "",
-  onValueChange: (v: string) => void,
-  render: (props: Record<string, any>) => N,
-  key: K
+  value: ValueOrGet<V>,
+  onValueChange: (v: V) => void,
+  render: (props: Record<string, any>) => E,
+  createCache: (n: E) => Cache<V>
 ) {
   //只是为了强制这个模块更新
   /**
@@ -68,7 +104,7 @@ function useTrigger<
   const onInput = props.onInput
   props.onInput = (e: any) => {
     if (getTriggerTime() == 'onInput') {
-      const newValue = input[key] || ''
+      const newValue = cache.getValue()
       updateVersion()
       onValueChange(newValue)
       onInput?.(e)
@@ -80,7 +116,7 @@ function useTrigger<
   const onBlur = props.onBlur
   props.onBlur = (e: any) => {
     if (getTriggerTime() == 'onBlur') {
-      const newValue = input[key] || ''
+      const newValue = cache.getValue()
       updateVersion()
       onValueChange(newValue)
       batchSignalEnd()
@@ -89,7 +125,9 @@ function useTrigger<
     }
   }
   const input = render(props)
-  useUpdateValue(value, input, key, version)
+  const cache = createCache(input)
+  // useUpdateValue2(cache, value, version);
+  useUpdateValue(value, cache, version)
   return input
 }
 
@@ -107,15 +145,47 @@ export function renderInput(
 ) {
   return useTrigger(triggerTime, props, value, onValueChange, props => {
     return renderFDom(type, props)
-  }, 'value') as any
+  }, InputCache.from) as any
 }
 
 
 
 export type ContentEditableProps<T extends DomElementType> = OrFun<FDomAttribute<T>> & BDomEvent<T> & {
-  triggerTime?: ValueOrGet<TriggerTime>,
-  value?: ValueOrGet<string>
+  triggerTime?: ValueOrGet<TriggerTime>
+  value?: ValueOrGet<string | number>
+  valueType?: "html" | "text"
   onValueChange(v: string): void
+}
+
+class ContentTextCache extends Cache<string | null> {
+  constructor(
+    private input: HTMLElement
+  ) { super() }
+  getValue() {
+    return this.input.textContent
+  }
+  setValue(v: string | null): void {
+    this.input.textContent = v
+    contentDidChange(this.input)
+  }
+  static from<T extends HTMLElement>(input: T) {
+    return new ContentTextCache(input)
+  }
+}
+class ContentHTMLCache extends Cache<string> {
+  constructor(
+    private input: HTMLElement
+  ) { super() }
+  getValue() {
+    return this.input.innerHTML
+  }
+  setValue(v: string): void {
+    this.input.innerHTML = v
+    contentDidChange(this.input)
+  }
+  static from<T extends HTMLElement>(input: T) {
+    return new ContentHTMLCache(input)
+  }
 }
 export function renderContentEditable<T extends DomElementType>(
   type: T,
@@ -125,16 +195,46 @@ export function renderContentEditable<T extends DomElementType>(
     value,
     onValueChange,
     triggerTime,
+    valueType = 'text',
     ...props
   } = arg as any
   return useTrigger(triggerTime, props, value, onValueChange, props => {
     return renderFDom(type, props as any)
-  }, 'textContent')
+  }, valueType == 'text' ? ContentTextCache.from : ContentHTMLCache.from)
+}
+
+//这里倒是跟input类似了,外部设值失败后光标始终到最后
+//但禁止设值,光标是不应该变动的..
+//仍然使用这个,因为光标可能在文字未变动时变化却观察不到
+function contentDidChange(v: HTMLElement) {
+  if (document.activeElement == v) {
+    //将光标移到最后
+    const range = document.createRange();
+    range.selectNodeContents(v);
+    range.collapse(false); // 让光标移动到 `range` 末尾
+    const selection = window.getSelection();
+    if (selection) {
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
 }
 
 
-
-
+class InputCheckCache extends Cache<boolean> {
+  constructor(
+    private input: HTMLInputElement
+  ) { super() }
+  getValue() {
+    return this.input.checked
+  }
+  setValue(v: boolean): void {
+    this.input.checked = v
+  }
+  static from(input: HTMLInputElement) {
+    return new InputCheckCache(input)
+  }
+}
 
 export type InputBoolProps = OrFun<Omit<FDomAttribute<'input'>, 'type'> & {
   type: "checkbox" | "radio"
@@ -161,6 +261,6 @@ export function renderInputBool({
       batchSignalEnd()
     },
   })
-  useUpdateValue(checked, input, 'checked', version)
+  useUpdateValue(checked, InputCheckCache.from(input), version)
   return input
 }
