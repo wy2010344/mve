afc53fd1630b90ea0519786211135b9120581dcb
wy2010344 <wangyang2010344@foxmail.com>
2025-03-31 18:48:18 +0800

    some fix

---
+++ dom-helper/src/centerPicker.ts
@@ -0,0 +1,125 @@
+import { renderForEach } from "mve-core"
+import { fdom } from "mve-dom"
+import { hookTrackSignal } from "mve-helper"
+import { animateSignal, pointerMoveDir } from "wy-dom-helper"
+import { addEffect, batchSignalEnd, circleFindNearst, circleFormat, createSignal, defaultSpringAnimationConfig, DeltaXSignalAnimationConfig, eventGetPageY, FrictionalFactory, GetValue, ScrollFromPage, StoreRef } from "wy-helper"
+
+
+
+
+export function defaultGetDistanceFromVelocity(velocity: number, deceleration?: number) {
+  return FrictionalFactory.get(deceleration).getFromVelocity(velocity).distance
+}
+export function centerPicker({
+  height,
+  cellHeight,
+  animationConfig = defaultSpringAnimationConfig,
+  getDistanceFromVelocity = defaultGetDistanceFromVelocity,
+  renderCell,
+  value,
+  realTimeValue = createSignal(value.get()),
+  circle
+}: {
+  height: GetValue<number>
+  getDistanceFromVelocity?(velocity: number): number
+  cellHeight: number
+  animationConfig?: DeltaXSignalAnimationConfig
+  renderCell(i: number): void,
+  value: StoreRef<number>
+  realTimeValue?: StoreRef<number>,
+  circle?: {
+    /**和0的距离,比如如果是从1开始,就是1*/
+    baseIndex?: number
+    /**总数量,比如12个月,就是12 */
+    count: number
+  }
+}) {
+  const scrollY = animateSignal(0)
+  function addValue(needAdd: number) {
+    const newValue = realTimeValue.get() + needAdd
+    if (circle) {
+      const circleDiff = circle.baseIndex || 0
+      realTimeValue.set(
+        circleFormat(newValue - circleDiff, circle.count) + circleDiff
+      )
+    } else {
+      realTimeValue.set(newValue)
+    }
+  }
+  function didChange() {
+    const needAdd = Math.floor(scrollY.get() / cellHeight)
+    if (needAdd) {
+      scrollY.silentDiff(-needAdd * cellHeight)
+      addValue(needAdd)
+      batchSignalEnd()
+    }
+  }
+  hookTrackSignal(value.get, function (v) {
+    let diff = (v - realTimeValue.get())
+    if (circle) {
+      diff = circleFindNearst(diff, circle.count)
+    }
+    if (diff) {
+      /**
+       * 这个对于周期的循环并不友好
+       */
+      addEffect(() => {
+        const snapTarget = diff * cellHeight
+        scrollY.changeTo(snapTarget,
+          animationConfig,
+          didChange)
+      })
+    }
+  })
+  return {
+    onPointerDown: pointerMoveDir(function (e, dir) {
+      if (dir == 'y') {
+        return ScrollFromPage.from(e, {
+          getPage: eventGetPageY,
+          scrollDelta(delta, velocity) {
+            scrollY.changeDiff(delta)
+            didChange()
+          },
+          onFinish(velocity) {
+            const distance = getDistanceFromVelocity(velocity)
+            const targetDis = distance + scrollY.get()
+            addValue(Math.round(targetDis / cellHeight))
+            const snapTarget = Math.round(targetDis / cellHeight) * cellHeight
+            scrollY.animateTo(
+              snapTarget,
+              animationConfig,
+              didChange
+            ).then(() => {
+              didChange()
+              value.set(realTimeValue.get())
+            })
+          }
+        })
+      }
+    }),
+    children() {
+      fdom.div({
+        s_transform() {
+          return `translateY(${-scrollY.get()}px)`
+        },
+        children() {
+          renderForEach<number, number, void>(function (callback) {
+            const v = realTimeValue.get()
+            //需要是奇数
+            const length = Math.ceil(height() / cellHeight)
+            //如果是偶数
+            const half = Math.floor(length / 2)
+            for (let i = v - half; i <= v + half; i++) {
+              let key = i
+              if (circle) {
+                const circleDiff = circle.baseIndex || 0
+                key = circleFormat(i - circleDiff, circle.count) + circleDiff
+              }
+              callback(key, key, renderCell)
+            }
+          })
+        }
+      })
+    }
+  }
+}
---
+++ dom-helper/src/pop.ts
@@ -0,0 +1,62 @@
+import { hookAddResult } from "mve-core";
+import { fdom } from "mve-dom";
+import { hookDestroy, renderArray } from "mve-helper";
+import { addEffect, createSignal, emptyArray, emptyFun, EmptyFun, SetValue } from "wy-helper";
+
+
+
+class PopFun<T> {
+  constructor(
+    readonly render: SetValue<SetValue<T>>,
+    private readonly onClose: SetValue<T>
+  ) { }
+  close = (value: T) => {
+    this.onClose(value)
+    popList.set(popList.get().filter(x => x != this))
+  }
+}
+const popList = createSignal<PopFun<any>[]>(emptyArray as any)
+export function createPop<T = any>(
+  callback: SetValue<SetValue<T>>,
+  onClose: SetValue<T> = emptyFun
+) {
+  const popFun = new PopFun(callback, onClose)
+  popList.set(popList.get().concat(popFun))
+  return popFun.close
+}
+function renderPopFun(pop: PopFun<any>) {
+  return pop.render(pop.close)
+}
+export function renderPop() {
+  renderArray(popList.get, renderPopFun)
+}
+
+
+
+export function hookExitAnimate<T extends HTMLElement>(
+  div: T,
+  operateClone: (div: T) => {
+    then(call: SetValue<any> | EmptyFun): any
+  }
+) {
+  hookDestroy(() => {
+    const rect = div.getBoundingClientRect()
+    //并不需要copy
+    const copy = div//div.cloneNode(true) as T
+    addEffect(() => {
+      createPop((close) => {
+        fdom.div({
+          className: 'fixed z-10',
+          s_left: rect.left + 'px',
+          s_top: rect.top + 'px',
+          children() {
+            hookAddResult(copy)
+          }
+        })
+        addEffect(() => {
+          operateClone(copy).then(close as EmptyFun)
+        })
+      })
+    })
+  })
+}
