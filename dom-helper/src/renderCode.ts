f5c85e0a48f50a34a3a3774bc860e28b1770045c
wy2010344 <wangyang2010344@foxmail.com>
2025-05-27 18:58:59 +0800

    update npm package

--- dom-helper/src/renderCode.ts
+++ dom-helper/src/renderCode.ts
@@ -1,10 +1,43 @@
-import { StoreRef, ValueOrGet } from "wy-helper";
+import { addEffect, createSignal, GetValue, SetValue, StoreRef, ValueOrGet } from "wy-helper";
 import { redo, undo, useContentEditable } from "./useContentEditable";
-import { appendRecord, contentDelete, ContentEditableModel, contentEnter, contentTab, getCurrentRecord, mb } from "wy-dom-helper/contentEditable";
+import { appendRecord, contentDelete, ContentEditableModel, contentEnter, contentTab, getCurrentRecord, initContentEditableModel, mb, getCurrentEditRecord, MbRange } from "wy-dom-helper/contentEditable";
 import { React } from "wy-dom-helper";
+import { hookTrackSignal } from "mve-helper";
 
 
-export function renderCode(model: StoreRef<ContentEditableModel>
+/**
+ * 受控组件模式
+ * @param get 
+ * @param set 
+ * @returns 
+ */
+export function renderCodeChange(
+  get: GetValue<string>,
+  onChange: (v: string, range: MbRange) => void
+) {
+  const model = createSignal(initContentEditableModel(get()))
+  hookTrackSignal(get, function (value) {
+    addEffect(() => {
+      const v = model.get()
+      const nv = getCurrentEditRecord(v).value
+      if (nv != value) {
+        //改变
+        model.set(initContentEditableModel(value))
+      }
+    })
+  })
+  return {
+    get: model.get,
+    set(v: ContentEditableModel) {
+      model.set(v)
+      const c = getCurrentEditRecord(v)
+      onChange(c.value, c.range)
+    }
+  }
+}
+
+export function renderCode(
+  model: StoreRef<ContentEditableModel>
 ) {
   const { current, renderContentEditable } = useContentEditable(model.get)
   return {
