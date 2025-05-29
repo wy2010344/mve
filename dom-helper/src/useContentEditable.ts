6ae626ec0b4640b9ec2f4aaa47c829e65d1e73fe
wy2010344 <wangyang2010344@foxmail.com>
2025-05-29 21:48:49 +0800

    add code

--- dom-helper/src/useContentEditable.ts
+++ dom-helper/src/useContentEditable.ts
@@ -3,35 +3,6 @@ import { ContentEditableModel, EditRecord, fixScroll, getCurrentEditRecord } fro
 import { addEffect, emptyFun, GetValue, ValueOrGet, valueOrGetToGet } from "wy-helper";
 
 
-export function redo(model: ContentEditableModel) {
-  const ndx = model.currentIndex + 1
-  if (ndx < model.history.length) {
-    return {
-      ...model,
-      currentIndex: ndx
-    }
-  }
-  return model
-}
-
-export function undo(model: ContentEditableModel) {
-  if (model.currentIndex) {
-    return {
-      ...model,
-      currentIndex: model.currentIndex - 1
-    }
-  }
-  return model
-}
-
-export function reset(model: ContentEditableModel, record: EditRecord) {
-  return {
-    currentIndex: 0,
-    history: [
-      record
-    ]
-  }
-}
 export function useContentEditable(getValue: GetValue<ContentEditableModel>) {
   function current() {
     const v = getValue()
