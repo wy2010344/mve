daaf1b2c706fe4015633154559397202d5a00622
wy2010344 <wangyang2010344@foxmail.com>
2025-06-16 19:13:30 +0800

    some code change

--- dom-helper/src/movePage.ts
+++ dom-helper/src/movePage.ts
@@ -1,6 +1,6 @@
 
-import { MoveEnd } from "wy-dom-helper"
-import { addEffect, AnimateSignal, batchSignalEnd, DeltaXSignalAnimationConfig, eventGetPageX, FrictionalFactory, GetValue, ScrollFromPage, spring, WeightMeasure } from "wy-helper"
+import { MoveEnd, pointerMoveDir } from "wy-dom-helper"
+import { addEffect, AnimateSignal, batchSignalEnd, Compare, DeltaXSignalAnimationConfig, eventGetPageX, eventGetPageY, FrictionalFactory, GetValue, memo, PointKey, ScrollFromPage, spring, WeightMeasure } from "wy-helper"
 import { defaultGetDistanceFromVelocity } from "./centerPicker"
 
 export function defaultGetPageSnap(velocity: number) {
@@ -13,6 +13,12 @@ export function defaultGetPageSnap(velocity: number) {
   })
 }
 
+export type MovePageProps = {
+  direction: PointKey
+  onMoveBegin?(): void
+  getDistanceFromVelocity?(velocity: number): number
+  callback: (direction: 1 | -1, velocity: number) => void
+}
 export function movePage(
   scroll: AnimateSignal,
   getSize: GetValue<number>,
@@ -21,54 +27,102 @@ export function movePage(
   //翻页时的全局速度
   let globalDirectionVelocity = 0
   return {
-    pointerDown(
-      initE: PointerEvent,
+    getOnPointerDown(
       {
-        getPage = eventGetPageX,
+        direction,
+        onMoveBegin,
         callback,
         getDistanceFromVelocity = defaultGetDistanceFromVelocity
-      }: {
-        getPage?(e: PointerEvent): number
-        getDistanceFromVelocity?(velocity: number): number
-        callback: (direction: 1 | -1, velocity: number) => void
-      }
-    ): MoveEnd<PointerEvent> {
-
-      return ScrollFromPage.from(initE, {
-        getPage,
-        scrollDelta(delta) {
-          scroll.changeDiff(delta)
-        },
-        onFinish(velocity) {
-          const distance = getDistanceFromVelocity(velocity)
-          const targetDis = distance + scroll.get()
-          const absTargetDis = Math.abs(targetDis)
-          if (absTargetDis < getSize() / 2) {
-            //恢复原状
-            scroll.changeTo(
-              0,
-              //效果不太好,有速度就变化
-              getPageSnap(velocity),
-            )
-          } else {
-            const direction = targetDis > 0 ? 1 : -1
-            globalDirectionVelocity = velocity
-            callback(direction, velocity)
-            batchSignalEnd()
+      }: MovePageProps
+    ) {
+      return pointerMoveDir(function () {
+        return {
+          onMove(e, dir) {
+            if (dir == direction) {
+              onMoveBegin?.()
+              return ScrollFromPage.from(e, {
+                getPage: direction == 'x' ? eventGetPageX : eventGetPageY,
+                scrollDelta(delta) {
+                  scroll.changeDiff(delta)
+                },
+                onFinish(velocity) {
+                  const distance = getDistanceFromVelocity(velocity)
+                  const targetDis = distance + scroll.get()
+                  const absTargetDis = Math.abs(targetDis)
+                  if (absTargetDis < getSize() / 2) {
+                    //恢复原状
+                    scroll.changeTo(
+                      0,
+                      //效果不太好,有速度就变化
+                      getPageSnap(velocity),
+                    )
+                  } else {
+                    const direction = targetDis > 0 ? 1 : -1
+                    globalDirectionVelocity = velocity
+                    callback(direction, velocity)
+                    batchSignalEnd()
+                  }
+                }
+              })
+            }
           }
         }
       })
     },
-    changePage(direction: number) {
-      addEffect(() => {
-        const diffSize = direction * getSize()
-        scroll.changeTo(
-          diffSize,
-          getPageSnap(globalDirectionVelocity),
-        )
-        globalDirectionVelocity = 0
-        scroll.silentDiff(-diffSize)
-      })
+    hookCompare<T>(getValue: GetValue<T>, compare: SortFun<T>) {
+      hookTrackSignal(memo<T>((lastValue, init) => {
+        const d = getValue()
+        if (init) {
+          const direction = Math.sign(compare(d, lastValue!))
+          if (direction) {
+            addEffect(() => {
+              const diffSize = direction * getSize()
+              scroll.changeTo(
+                diffSize,
+                getPageSnap(globalDirectionVelocity),
+              )
+              globalDirectionVelocity = 0
+              scroll.silentDiff(-diffSize)
+            })
+          }
+        }
+        return d
+      }))
     }
+  }
+}
+export type SortFun<T> = (a: T, b: T) => number
+import { hookTrackSignal } from "mve-helper"
+
+
+export type MovePageConfig<T> = {
+  scroll: AnimateSignal
+  getSize?(): number,
+  getValue(): T,
+  /**
+   * a>b 返回1
+   * a<b 返回-1
+   * @param a 
+   * @param b 
+   */
+  compare: SortFun<T>
+} & MovePageProps
+export function hookSimpleMovePage<T>(container: HTMLElement, {
+  scroll,
+  getSize,
+  getValue,
+  compare,
+  ...args
+}: MovePageConfig<T>) {
+  const mp = movePage(scroll, getSize
+    || (args.direction == 'x'
+      ? (() => container.clientWidth)
+      : (() => container.clientHeight)))
+  container.addEventListener('pointerdown', mp.getOnPointerDown(args))
+  mp.hookCompare(getValue, compare)
+}
+export function pluginSimpleMovePage<T>(config: MovePageConfig<T>) {
+  return function (container: HTMLElement) {
+    return hookSimpleMovePage(container, config)
   }
 }
