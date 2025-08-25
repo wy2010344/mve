632722389e96e5d631e235f294f7a2dc2c02d0e3
wy2010344 <wangyang2010344@foxmail.com>
2025-08-25 18:24:30 +0800

    little fix

--- daisy-mobile-helper/vite.config.ts
+++ daisy-mobile-helper/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 export default defineConfig({
@@ -7,25 +7,25 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: {
-        index: resolve(__dirname, 'src/index.ts')
+        index: resolve(__dirname, 'src/index.ts'),
       },
-      formats: ["es", "cjs"]
+      formats: ['es', 'cjs'],
     },
     minify: false,
     rollupOptions: {
       external: [
         /^wy-helper(\/)?/,
         /^wy-dom-helper(\/)?/,
-        "mve-core",
-        "mve-helper",
-        "mve-dom",
-        "mve-dom-helper",
+        'mve-core',
+        /^mve-helper(\/)?/,
+        'mve-dom',
+        /^mve-dom-helper(\/)?/,
         'history',
         'motion',
         /^mve-icons(\/)?/,
-        'tyme4ts'
-      ]
-    }
+        'tyme4ts',
+      ],
+    },
   },
   plugins: [
     dts({
@@ -36,5 +36,5 @@ export default defineConfig({
       // skipDiagnostics: false
     }),
     // tailwindcss()
-  ]
-});
+  ],
+})
