c5d3bd836158e9c11c38b4500f29b4b8b09a3e61
wy2010344 <wangyang2010344@foxmail.com>
2025-09-11 18:22:45 +0800

    fix dom move

--- dom-helper/src/pop.ts
+++ dom-helper/src/pop.ts
@@ -1,26 +1,38 @@
-import { hookAddResult } from "mve-core";
-import { fdom } from "mve-dom";
-import { hookDestroy, renderArray } from "mve-helper";
-import { cns } from "wy-dom-helper";
-import { addEffect, createSignal, emptyArray, emptyFun, EmptyFun, SetValue, StoreRef } from "wy-helper";
+import { closeSync } from 'fs'
+import { hookAddResult } from 'mve-core'
+import { fdom } from 'mve-dom'
+import { hookDestroy, renderArray } from 'mve-helper'
+import { cns } from 'wy-dom-helper'
+import {
+  addEffect,
+  circleFindNearst,
+  circleFormat,
+  createSignal,
+  emptyArray,
+  emptyFun,
+  EmptyFun,
+  GetValue,
+  removeEqual,
+  SetValue,
+  StoreRef,
+} from 'wy-helper'
 
-
-
-class PopFun<T> {
+class PopFun<T, M = T> {
   constructor(
-    readonly render: SetValue<SetValue<T>>,
+    readonly render: SetValue<SetValue<M>>,
     private readonly onClose: SetValue<T>,
     private readonly popList: StoreRef<PopFun<any>[]>
-  ) { }
+  ) {}
   close = (value: T) => {
     this.onClose(value)
-    this.popList.set(this.popList.get().filter(x => x != this))
+    this.popList.set(this.popList.get().filter((x) => x != this))
   }
 }
 
-
+function renderPopFun(pop: PopFun<any>) {
+  return pop.render(pop.close)
+}
 export function createPopList() {
-
   const popList = createSignal<PopFun<any>[]>(emptyArray as any)
   function createPop<T = any>(
     callback: SetValue<SetValue<T>>,
@@ -30,27 +42,110 @@ export function createPopList() {
     popList.set(popList.get().concat(popFun))
     return popFun.close
   }
-  function renderPopFun(pop: PopFun<any>) {
-    return pop.render(pop.close)
-  }
   function renderPop() {
     renderArray(popList.get, renderPopFun)
   }
 
   return {
     renderPop,
-    createPop
+    createPop,
   }
 }
 
-export const { renderPop, createPop } = createPopList()
+type PopWithRearrangeInfo<T> = {
+  callback(info: PopWithRearrange<T>): void
+  info: PopWithRearrange<T>
+  callTime: number
+}
+export type PopWithRearrange<T> = {
+  closeSet: Set<SetValue<T>>
+  closeIt: SetValue<T>
+  size: GetValue<number>
+  getIndex: GetValue<number>
+  setIndex(n: number): void
+  callTime(): number
+}
+export function createPopListWithRearrange() {
+  const popList = createSignal<PopWithRearrangeInfo<any>[]>(emptyArray as any)
+  const arrangeList = createSignal<PopWithRearrangeInfo<any>[]>(
+    emptyArray as any
+  )
 
+  function size() {
+    return arrangeList.get().length
+  }
+
+  function createPop<T = any>(
+    /**需要唯一性*/
+    callback: (info: PopWithRearrange<T>) => void,
+    index: number = -1
+  ) {
+    const old = popList.get().find((x) => x.callback == callback)
+    if (old) {
+      old.info.setIndex(index)
+      old.callTime++
+      return old.info
+    }
+    function getIndex() {
+      return arrangeList.get().findIndex((v) => v == popFun)
+    }
+    function notSelf(x: PopWithRearrangeInfo<T>) {
+      return x != popFun
+    }
+    const closeSet = new Set<SetValue<T>>()
+    const info: PopWithRearrange<T> = {
+      closeSet,
+      closeIt(v) {
+        closeSet.forEach((closeIt) => {
+          closeIt(v)
+        })
+        popList.set(popList.get().filter(notSelf))
+        arrangeList.set(arrangeList.get().filter(notSelf))
+      },
+      callTime() {
+        return popFun.callTime
+      },
+      size,
+      getIndex,
+      setIndex(n) {
+        const list = arrangeList.get().slice()
+        n = circleFormat(n, list.length)
+        removeEqual(list, popFun)
+        list.splice(n, 0, popFun)
+        arrangeList.set(list)
+      },
+    }
+    const popFun: PopWithRearrangeInfo<T> = {
+      callback,
+      info,
+      callTime: 1,
+    }
+    popList.set(popList.get().concat(popFun))
+    const list = arrangeList.get().slice()
+    index = circleFormat(index, list.length + 1)
+    list.splice(index, 0, popFun)
+    arrangeList.set(list)
+    return info
+  }
+  function renderPop() {
+    renderArray(popList.get, renderPopArrangeFun)
+  }
+
+  return {
+    renderPop,
+    createPop,
+  }
+}
+function renderPopArrangeFun(pop: PopWithRearrangeInfo<any>) {
+  return pop.callback(pop.info)
+}
+export const { renderPop, createPop } = createPopList()
 
 export function hookExitAnimate<T extends HTMLElement>(
   div: T,
   {
     className,
-    operateClone
+    operateClone,
   }: {
     className?: string
     operateClone: (div: T) => {
@@ -61,7 +156,7 @@ export function hookExitAnimate<T extends HTMLElement>(
   hookDestroy(() => {
     const rect = div.getBoundingClientRect()
     //并不需要copy
-    const copy = div//div.cloneNode(true) as T
+    const copy = div //div.cloneNode(true) as T
     addEffect(() => {
       createPop((close) => {
         //可能会出现更多自定义样式
@@ -73,7 +168,7 @@ export function hookExitAnimate<T extends HTMLElement>(
           s_height: rect.height + 'px',
           children() {
             hookAddResult(copy)
-          }
+          },
         })
         addEffect(() => {
           operateClone(copy).then(close as EmptyFun)
--- dom-helper/src/useContentEditable.ts
+++ dom-helper/src/useContentEditable.ts
@@ -1,7 +1,17 @@
-import { hookTrackSignal, renderOne } from "mve-helper";
-import { ContentEditableModel, EditRecord, fixScroll, getCurrentEditRecord } from "wy-dom-helper/contentEditable";
-import { addEffect, emptyFun, GetValue, ValueOrGet, valueOrGetToGet } from "wy-helper";
-
+import { hookTrackSignal, renderOne } from 'mve-helper'
+import {
+  ContentEditableModel,
+  EditRecord,
+  fixScroll,
+  getCurrentEditRecord,
+} from 'wy-dom-helper/contentEditable'
+import {
+  addEffect,
+  emptyFun,
+  GetValue,
+  ValueOrGet,
+  valueOrGetToGet,
+} from 'wy-helper'
 
 export function useContentEditable(getValue: GetValue<ContentEditableModel>) {
   function current() {
@@ -10,25 +20,29 @@ export function useContentEditable(getValue: GetValue<ContentEditableModel>) {
   }
   return {
     current,
+
     renderContentEditable({
       noFocus,
-      children
+      children,
     }: {
       noFocus?: ValueOrGet<boolean>
       children(value: string): HTMLElement
     }) {
       const nF = valueOrGetToGet(noFocus)
-      renderOne(() => current().value, function (value) {
-        const div = children(value)
-        hookTrackSignal(() => {
-          if (nF()) {
-            return
-          }
-          addEffect(() => {
-            fixScroll(div, current())
+      renderOne(
+        () => current().value,
+        function (value) {
+          const div = children(value)
+          hookTrackSignal(() => {
+            if (nF()) {
+              return
+            }
+            addEffect(() => {
+              fixScroll(div, current())
+            })
           })
-        }, emptyFun)
-      })
-    }
+        }
+      )
+    },
   }
 }
