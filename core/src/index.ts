115e0110cbf2865f2608208f6eeb139ef85d7670
wy2010344 <wangyang2010344@foxmail.com>
2025-09-25 18:22:51 +0800

    rename mdom to kdom

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,18 +1,22 @@
 import { batchSignalEnd, EmptyFun, GetValue } from 'wy-helper'
-import { hookAlterChildren, hookAlterStateHolder, hookCurrentStateHolder } from './cache'
+import {
+  hookAlterChildren,
+  hookAlterStateHolder,
+  hookCurrentStateHolder,
+} from './cache'
 import { StateHolder } from './stateHolder'
 
 export * from './renderForEach'
 export {
-  hookAddResult, hookAddDestroy, hookAlterChildren,
+  hookAddResult,
+  hookAddDestroy,
+  hookAlterChildren,
   hookAlterStateHolder,
-  hookCurrentStateHolder
+  hookCurrentStateHolder,
 } from './cache'
 export { createContext } from './context'
 export type { Context } from './context'
 export * from './hookChildren'
-
-
 
 const globalHolder = new StateHolder()
 export function runGlobalHolder(fun: EmptyFun) {
@@ -25,13 +29,12 @@ export function destroyGlobalHolder() {
   globalHolder.destroy()
 }
 
-
 export function render(create: EmptyFun) {
   const stateHolder = new StateHolder(globalHolder)
   const before = hookAlterStateHolder(stateHolder)
   create()
+  batchSignalEnd() // 必须放在之前
   hookAlterStateHolder(before)
-  batchSignalEnd()
   return function () {
     stateHolder.removeFromParent()
   }
@@ -41,13 +44,9 @@ export function hookIsDestroyed() {
   return hookCurrentStateHolder()!.destroyed
 }
 
-
 export function renderStateHolder<T>(fun: GetValue<T>) {
   const c = hookCurrentStateHolder()!
-  const before = hookAlterStateHolder(new StateHolder(
-    c,
-    c.contexts.length
-  ))
+  const before = hookAlterStateHolder(new StateHolder(c, c.contexts.length))
   const o = fun()
   hookAlterStateHolder(before)
   return o
